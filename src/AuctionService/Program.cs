09248fe296ff01c0a347b4e3b51d970182b03769
Jose Perdomo <jose.perdomo.dev@gmail.com>
2023-11-09 22:29:40 -0700

    Add Automapper for DTOs

--- src/AuctionService/Program.cs
+++ src/AuctionService/Program.cs
@@ -9,7 +9,7 @@ builder.Services.AddControllers();
 builder.Services.AddDbContext<AuctionDbContext>(opt => {
     opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultCOnnection"));
 });
-
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
